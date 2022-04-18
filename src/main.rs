4479b27294f30b40e33a247f1f97cd6eeac6c831
Rasmus Lövegren <rasmus@lovegren.fi>
2022-04-18 22:25:30 +0300

    Tweak frame detection

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use std::{
-    ops::Index,
-    time::{Duration, SystemTime},
-};
+use std::time::{Duration, Instant};
 
 use opencv::{
-    core::{self, Moments, Point, Scalar},
-    highgui,
-    imgcodecs::IMREAD_COLOR,
-    imgproc::{self, INTER_LINEAR},
+    core::{self, Point, Scalar},
+    highgui, imgproc,
     prelude::*,
     types::VectorOfMat,
     videoio, Result,
@@ -16,13 +11,33 @@ use serialport::SerialPort;
 
 const CONTROLS_WINDOW: &str = "controls";
 const MANUAL_MODE: bool = false;
-const CENTER_TOLERANCE: f64 = 5.0;
-const CENTER_TOLERANCE_MISSED: f64 = 8.0;
+
+/// How many pixels one step corresponds to
 const PX_PER_STEP: f64 = 4.5;
-const MOP_RECT_HEIGHT_PCT: f64 = 1.0;
-// const NEXT_FRAME_SKIP_STEPS: usize = 2825;
+
+/// Extra time to wait before taking photo. Helps avoids vibrations during exposure.
+const PRE_SHUTTER_WAIT_MSEC: u64 = 300;
+
+/// Account for camera video feed -> opencv input lag
+const INPUT_LAG_MSEC: u64 = 300;
+
+/// How many milliseconds it takes to move the film 1000 steps
+const MSEC_PER_1000_STEPS: u64 = 700;
+
+/// Time it takes for stepper motor to (de)accelerate between zero and max speed
+/// (maxSpeed / acceleration)
+const STEPPER_ACCEL_TIME: u64 = 120;
+
+/// Account for camera not responding to shutter button press instantly
+const SHUTTER_EXTRA_WAIT_MSEC: u64 = 200;
+
+/// How many steps to move the film forward after taking a photo
 const NEXT_FRAME_SKIP_STEPS: usize = 2500;
 
+///
+const NEW_DETECTION_POS: f64 = 150.0;
+
+#[derive(Debug)]
 struct State {
     current: ScannerState,
 }
@@ -30,24 +45,24 @@ struct State {
 #[derive(Debug)]
 enum ScannerState {
     AligningFrame {
-        init_timestamp: SystemTime,
+        wait_until: Instant,
         last_seen_x: Option<f64>,
     },
     TakingPhoto {
-        init_timestamp: SystemTime,
+        wait_until: Instant,
     },
     WaitingForShutterOpen {
-        init_timestamp: SystemTime,
+        wait_until: Instant,
     },
     SkipToNextFrame {
-        init_timestamp: SystemTime,
+        wait_until: Instant,
     },
 }
 
 impl Default for ScannerState {
     fn default() -> Self {
         ScannerState::AligningFrame {
-            init_timestamp: SystemTime::now() - Duration::from_secs(1),
+            wait_until: Instant::now(),
             last_seen_x: None,
         }
     }
@@ -68,7 +83,6 @@ impl State {
 }
 
 struct Contour {
-    contour: Mat,
     area: f64,
     end_x: f64,
 }
@@ -102,7 +116,7 @@ impl Scanner {
         }
     }
 
-    pub fn move_forward(&mut self, steps: usize, silent: bool) {
+    pub fn move_forward(&mut self, steps: usize, _silent: bool) {
         // 1300 ~= one frame
         self.write(&format!("-{}M", steps));
         self.prev_dir = true;
@@ -203,7 +217,10 @@ fn main() -> Result<()> {
     println!("u = Stop focus");
 
     let mut scanner = Scanner::new(port);
+
+    println!("Toggling camera focus...");
     scanner.focus();
+    std::thread::sleep(Duration::from_millis(500));
     scanner.stop_focus();
 
     let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?; // 0 is the default camera
@@ -236,7 +253,7 @@ fn main() -> Result<()> {
     highgui::create_trackbar(
         "height",
         CONTROLS_WINDOW,
-        &mut (frame_height - 0),
+        &mut frame_height.clone(),
         frame_height,
         None,
     )?;
@@ -253,7 +270,7 @@ fn main() -> Result<()> {
         let y = highgui::get_trackbar_pos("y", CONTROLS_WINDOW)?;
         let width = highgui::get_trackbar_pos("width", CONTROLS_WINDOW)?;
         let height = highgui::get_trackbar_pos("height", CONTROLS_WINDOW)?;
-        let param = highgui::get_trackbar_pos("param", CONTROLS_WINDOW)?;
+        // let param = highgui::get_trackbar_pos("param", CONTROLS_WINDOW)?;
 
         // Don't allow cropping outside image (crashes opencv)
         let x = x.min(frame_width - width);
@@ -391,12 +408,11 @@ fn main() -> Result<()> {
                 let seen_end = bbox.x as f64 + seen_width;
 
                 Contour {
-                    contour,
                     area,
                     end_x: seen_end,
                 }
             })
-            // .filter(|moments| moments.area > 100_000.0 * MOP_RECT_HEIGHT_PCT)
+            // .filter(|moments| moments.area > 100_000.0)
             .collect();
 
         cwms.sort_by(|a, b| a.area.partial_cmp(&b.area).unwrap());
@@ -406,10 +422,6 @@ fn main() -> Result<()> {
         // let last_cwm = cwms.first();
 
         // dbg!(last_cwm.map(|cwm| cwm.end_x));
-
-        let center = (frame_cropped.cols() as f64) / 2.0;
-        let missed = center - CENTER_TOLERANCE_MISSED;
-        let in_position = center + CENTER_TOLERANCE;
 
         let mut frame_inv = Mat::default();
         core::bitwise_not(&frame, &mut frame_inv, &core::no_array()?)?;
@@ -459,7 +471,7 @@ fn main() -> Result<()> {
 
         if key == 't' {
             state.set(ScannerState::TakingPhoto {
-                init_timestamp: SystemTime::now() - Duration::from_secs(1),
+                wait_until: Instant::now(),
             });
             pause = false;
         }
@@ -478,25 +490,29 @@ fn main() -> Result<()> {
         if !pause && !MANUAL_MODE {
             match state.current {
                 ScannerState::AligningFrame {
-                    init_timestamp,
+                    wait_until,
                     last_seen_x,
                 } => {
                     match last_cwm {
                         Some(last_cwm) => {
                             // Always update last seen x position to state
                             state.set(ScannerState::AligningFrame {
-                                init_timestamp,
+                                wait_until,
                                 last_seen_x: Some(last_cwm.end_x),
                             });
 
                             // Immediately stop film if frame moves past
                             // detection point
-                            let stop_pos = 150.0;
                             if let Some(last_seen_x) = last_seen_x {
-                                if last_seen_x >= stop_pos && last_cwm.end_x < stop_pos {
+                                let moved_past_detection_point = last_seen_x >= NEW_DETECTION_POS
+                                    && last_cwm.end_x < NEW_DETECTION_POS;
+
+                                if moved_past_detection_point {
                                     scanner.stop(true);
                                     state.set(ScannerState::AligningFrame {
-                                        init_timestamp: SystemTime::now(),
+                                        wait_until: Instant::now()
+                                            + Duration::from_millis(INPUT_LAG_MSEC)
+                                            + Duration::from_millis(STEPPER_ACCEL_TIME),
                                         last_seen_x: Some(last_cwm.end_x),
                                     });
 
@@ -505,7 +521,7 @@ fn main() -> Result<()> {
                             }
 
                             // Wait some time after previous move command
-                            if init_timestamp.elapsed().unwrap().as_secs_f64() < 1.0 {
+                            if Instant::now() < wait_until {
                                 continue;
                             }
 
@@ -513,50 +529,67 @@ fn main() -> Result<()> {
                             // towards end of gap
                             let steps = last_cwm.end_x * PX_PER_STEP;
                             let steps = steps.round(); //.max(5.0);
-                            let steps = steps + 100.0;
+                            let steps = steps + 50.0;
                             println!(
                                 "{} px to end of frame gap, moving motor {} steps",
                                 last_cwm.end_x, steps
                             );
                             scanner.move_forward(steps as usize, true);
+
+                            let step_time = MSEC_PER_1000_STEPS as f64 / 1000.0 * steps;
+                            let wait_time = Duration::from_millis(INPUT_LAG_MSEC)
+                                + Duration::from_millis(STEPPER_ACCEL_TIME)
+                                + Duration::from_millis(STEPPER_ACCEL_TIME)
+                                + Duration::from_millis(step_time as u64);
+
+                            // Limit wait time to 1 sec
+                            let wait_time = wait_time.min(Duration::from_secs(1));
+
                             state.set(ScannerState::AligningFrame {
-                                init_timestamp: SystemTime::now(),
+                                wait_until: Instant::now() + wait_time,
                                 last_seen_x: Some(last_cwm.end_x),
                             })
                         }
                         None => {
                             // Wait some time after previous move command
-                            if init_timestamp.elapsed().unwrap().as_secs_f64() < 1.0 {
+                            if Instant::now() < wait_until {
                                 continue;
                             }
 
                             // Could not find frame gaps, assume frame is
                             // aligned and take photo
 
-                            println!("Unable to find frame gap, taking photo");
-                            scanner.stop(false);
+                            println!("No frame gaps in feed, stopping motor and taking photo");
+                            scanner.stop(true);
+                            let wait_time = Duration::from_millis(INPUT_LAG_MSEC)
+                                + Duration::from_millis(PRE_SHUTTER_WAIT_MSEC)
+                                + Duration::from_millis(STEPPER_ACCEL_TIME);
+
                             state.set(ScannerState::TakingPhoto {
-                                init_timestamp: SystemTime::now(),
+                                wait_until: Instant::now() + wait_time,
                             })
                         }
                     }
                 }
-                ScannerState::TakingPhoto { init_timestamp } => {
+                ScannerState::TakingPhoto { wait_until } => {
                     // Wait until enough time has passed so that the film has come to a stop
-                    if init_timestamp.elapsed().unwrap().as_secs_f64() < 1.0 {
+                    if Instant::now() < wait_until {
                         continue;
                     }
 
                     scanner.take_photo();
                     scanner.stop_focus();
 
+                    let wait_time = Duration::from_millis(INPUT_LAG_MSEC)
+                        + Duration::from_millis(SHUTTER_EXTRA_WAIT_MSEC);
+
                     state.set(ScannerState::WaitingForShutterOpen {
-                        init_timestamp: SystemTime::now(),
+                        wait_until: Instant::now() + wait_time,
                     });
                 }
-                ScannerState::WaitingForShutterOpen { init_timestamp } => {
+                ScannerState::WaitingForShutterOpen { wait_until } => {
                     // Wait until enough time has passed so that we can see the camera shutter black-out
-                    if init_timestamp.elapsed().unwrap().as_secs_f64() < 1.0 {
+                    if Instant::now() < wait_until {
                         continue;
                     }
 
@@ -566,19 +599,30 @@ fn main() -> Result<()> {
                     // shutter black-out)
                     let non_zero_px = core::count_non_zero(&frame_bw)?;
                     if non_zero_px > 2000 {
+                        println!("Moving film to next frame");
+
                         // Move enough forward so that we start detecting the next frame
                         scanner.move_forward(NEXT_FRAME_SKIP_STEPS, false);
 
+                        let step_time =
+                            MSEC_PER_1000_STEPS as f64 / 1000.0 * NEXT_FRAME_SKIP_STEPS as f64;
+
+                        let wait_time = Duration::from_millis(INPUT_LAG_MSEC)
+                            + Duration::from_millis(STEPPER_ACCEL_TIME)
+                            + Duration::from_millis(step_time as u64);
+
                         state.set(ScannerState::SkipToNextFrame {
-                            init_timestamp: SystemTime::now(),
+                            wait_until: Instant::now() + wait_time,
                         });
+                    } else {
+                        println!("Waiting for end of shutter black-out")
                     }
                 }
 
-                ScannerState::SkipToNextFrame { init_timestamp } => {
+                ScannerState::SkipToNextFrame { wait_until } => {
                     // scanner.focus();
                     // Wait until enough time has passed so that the film has moved far enough
-                    if init_timestamp.elapsed().unwrap().as_secs_f64() < 3.0 {
+                    if Instant::now() < wait_until {
                         continue;
                     }
 
